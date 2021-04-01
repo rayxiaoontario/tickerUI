2b515c5a4ffb421b2a84ae7d8620ce41b8dd53a3
ray xiao <rayxiao@MacBooks-MacBook-Pro.local>
2021-04-01 13:18:26 -0400

    introduce interface.

--- ticker-scan/src/app/models/ticker.model.ts
+++ ticker-scan/src/app/models/ticker.model.ts
@@ -1,6 +1,12 @@
 
+
 export interface ITickerOnPanel {
     id : String ;
     checked : Boolean ;
     opened : Boolean ;
+}
+
+export interface IFlaggedTicker { 
+    name : String ;
+    flagDate : Array<String> ;
 }
